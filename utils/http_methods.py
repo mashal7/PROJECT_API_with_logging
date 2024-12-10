e71334d921459ef15ea024ac1fcacdf360b6ceed
mashal7 <mashal7@mail.ru>
2024-12-10 15:52:15 +0300

    new

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -12,44 +12,44 @@ class HttpMethods:
     '''Список Http методов'''
 
     headers = {'Content-Type': 'application/json'}
-    cookie = ''
+    cookie = {}
 
     @staticmethod
     def get(url):
         with allure.step('GET'):
             CustomLogger.add_request(url, method='get')
             start_logs(logger, url, method='get')
-            result = requests.get(url, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+            result = requests.get(url, headers=HttpMethods.headers, cookies=HttpMethods.cookie, timeout=10)
             end_logs(logger, result)
             CustomLogger.add_response(result)
             return result
 
     @staticmethod
-    def post(url, body):
+    def post(url, body=None):
         with allure.step('POST'):
             CustomLogger.add_request(url, method='post')
             start_logs(logger, url, method='post')
-            result = requests.post(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+            result = requests.post(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie, timeout=10)
             end_logs(logger, result)
             CustomLogger.add_response(result)
             return result
 
     @staticmethod
-    def put(url, body):
+    def put(url, body=None):
         with allure.step('PUT'):
             CustomLogger.add_request(url, method='put')
             start_logs(logger, url, method='put')
-            result = requests.put(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+            result = requests.put(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie, timeout=10)
             end_logs(logger, result)
             CustomLogger.add_response(result)
             return result
 
     @staticmethod
-    def delete(url, body):
+    def delete(url, body=None):
         with allure.step('DELETE'):
             CustomLogger.add_request(url, method='delete')
             start_logs(logger, url, method='delete')
-            result = requests.put(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+            result = requests.put(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie, timeout=10)
             end_logs(logger, result)
             CustomLogger.add_response(result)
             return result
